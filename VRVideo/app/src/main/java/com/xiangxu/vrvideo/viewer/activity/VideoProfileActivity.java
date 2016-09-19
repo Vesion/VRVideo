a3a87cbd0f290edaa0c4b82381be325a9ce5baa2
Vesion <xuxiang19930924@126.com>
2016-09-19 13:01:47 +0800

    Nothing

--- VRVideo/app/src/main/java/com/xiangxu/vrvideo/viewer/activity/VideoProfileActivity.java
+++ VRVideo/app/src/main/java/com/xiangxu/vrvideo/viewer/activity/VideoProfileActivity.java
@@ -5,15 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
 import com.xiangxu.vrvideo.R;
 import com.xiangxu.vrvideo.util.LLog;
-import com.xiangxu.vrvideo.viewer.unity.UnityPlayerActivity;
+import com.xiangxu.vrvideo.unity.UnityPlayerActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
